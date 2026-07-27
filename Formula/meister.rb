72ba975fa35facecd048dee519396fbfe5f1aa75
Marco Föllmer <marco.foellmer@ebf.com>
2026-07-27 04:56:37 +0200

    formula: update SHA256 for v6.5

--- Formula/meister.rb
+++ Formula/meister.rb
@@ -1,10 +1,10 @@
 class Meister < Formula
   desc "macOS Maintenance, Self-Healing & Dotfiles Sync (meister + meisterSiri)"
   homepage "https://github.com/maf4711/homebrew-meister"
-  url "https://github.com/maf4711/homebrew-meister/archive/refs/tags/v6.4.tar.gz"
-  sha256 "54ccdf57fd33694dd9910777b575c32ece095fbb5e4b672b9a754cd5f05240f1"
+  url "https://github.com/maf4711/homebrew-meister/archive/refs/tags/v6.5.tar.gz"
+  sha256 "8cd098ea37fd3ce43fa0903acd832c6dde4328c6315dfbecbfef0eeaaf185d5d"
   license "GPL-3.0-only"
-  version "6.4"
+  version "6.5"
 
   depends_on :macos
 
