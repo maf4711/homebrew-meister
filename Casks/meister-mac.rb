0cf365ad2b35e2a8477551902b3ba71b0ad89c7a
marco <foellmer@mac.com>
2026-09-17 16:04:09 +0200

    cask: MeisterSiri.app v6.25

--- Casks/meister-mac.rb
+++ Casks/meister-mac.rb
@@ -1,5 +1,5 @@
 cask "meister-mac" do
-  version "6.24"
+  version "6.25"
   sha256 "4d5d2dc37b89835f3874a36e4232476337b466ef7b4e9f18bffa8dde0c3f689b"
 
   url "https://github.com/maf4711/homebrew-meister/releases/download/v#{version}/MeisterSiri-macOS.zip"
